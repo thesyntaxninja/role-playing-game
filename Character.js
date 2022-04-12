5a16e8f38559e1f3bd0edd729a550c63d1bd1ff0
thesyntaxninja <pdesmay@gmail.com>
2022-04-11 18:36:49 -0700

    add health bar

--- Character.js
+++ Character.js
@@ -1,8 +1,13 @@
 import {getDicePlaceHolderHtml, getRollDiceArray} from "./utils.js"
 
+const getPercentage = (reamainingHealth, maximumHealth) => {
+    return (100 * reamainingHealth) / maximumHealth
+}
 
 function Character(data) {
     Object.assign(this, data)
+
+    this.maxHealth = this.health
 
     this.diceArray = getDicePlaceHolderHtml(this.diceCount)
 
@@ -11,6 +16,16 @@ function Character(data) {
         return this.diceArray = this.currentDiceScore.map((num) => {
             return `<div class="dice">${num}</div>`
         }).join("")
+    }
+
+    this.getHealthBarHtml = () => {
+        const percent = getPercentage(this.health, this.maxHealth)
+        return `
+        <div class="health-bar-outer">
+            <div class="health-bar-inner ${percent < 26 ? "danger" : ""} " 
+            style="width: ${percent}%;">
+            </div>
+        </div>`
     }
 
     this.takeDamage = (attackScoreArray) => {
@@ -27,12 +42,13 @@ function Character(data) {
 
     this.getCharacterHtml = () => {
         const {elementId, name, avatar, health, diceCount, diceArray} = this
-
+        const healthBar = this.getHealthBarHtml()
         return (`
             <div class="character-card">
                 <h4 class="name">${name}</h4>
                 <img class="avatar" src=${avatar} />
                 <p class="health">health: <b> ${health} </b></p>
+                ${healthBar}
                 <div class="dice-container">
                     ${diceArray}
                 </div>
